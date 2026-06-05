3f66f69c80bdc1706899bd29d8ed8723d648ff86
Rxn321 <liuryan112255@gmail.com>
2026-06-05 14:02:15 -0700

    addded vector

---
+++ src/Vector.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <cmath>
+
+struct Vec2
+{
+    float x;
+    float y;
+
+    Vec2();
+    Vec2(float x, float y);
+
+    Vec2 operator+(const Vec2& other) const;
+    Vec2 operator-(const Vec2& other) const;
+    Vec2 operator*(float scalar) const;
+    Vec2 operator/(float scalar) const;
+
+    Vec2& operator+=(const Vec2& other);
+    Vec2& operator-=(const Vec2& other);
+    Vec2& operator*=(float scalar);
+    Vec2& operator/=(float scalar);
+
+    float Magnitude() const;
+    float MagnitudeSquared() const;
+    Vec2 Normalize() const;
+    float Dot(const Vec2& other) const;
+    float Distance(const Vec2& other) const;
+};
